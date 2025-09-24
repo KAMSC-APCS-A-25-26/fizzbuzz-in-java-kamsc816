b3911a3c721e84a66b7e021f996d5c7d7c095a5f
kamsc816 <03samuel.miller@kamsc.org>
2025-09-24 09:52:43 -0400

    Update FizzBuzz.java

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -9,25 +9,20 @@ public class FizzBuzz {
      * Output should be 100 whitespace-separated tokens (spaces or newlines are fine).
      */
     public static void main(String[] args) {
-        for(int i=0; i<100; i++)
+        for(int i=1; i<101; i++)
             {
-                if(i%3=0)
-                {
+                if(i%3==0 && i%5==0){
+                    System.out.println("FizzBuzz");
+                }
+                else if(i%3==0){
                     System.out.println("Fizz");
                 }
-                elif(i%5=0)
-                {
+                else if(i%5==0){
                     System.out.println("Buzz");
-                }
-                elif(i%3=0 + i%5=0)
-                {
-                    System.out.println("FizzBuzz");
                 }
                 else{
                     System.out.println(i);
-                }
-                
-                
+                }   
             }
     }
 }
